b23e9c8510e638591acd9c1816c57b2ac6d50d5f
afinievskym <afinievskym@gmail.com>
2018-02-04 21:51:12 +1000

    Пробую сделать подключение бд через RecyclerView

--- app/src/main/java/com/company/afinievskym/letsdoit/Help.java
+++
@@ -1,9 +0,0 @@
-package com.company.afinievskym.letsdoit;
-
-/**
- * Created by Programmer on 03.02.2018.
- */
-
-public class Help {
-
-}
--- app/src/main/java/com/company/afinievskym/letsdoit/MainActivity.java
+++ app/src/main/java/com/company/afinievskym/letsdoit/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -33,6 +35,11 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(task);
             }
         });
+        //Создание RecyclerView
+        RecyclerView rv = findViewById(R.id.rv);
+        LinearLayoutManager llm = new LinearLayoutManager(MainActivity.this);
+        rv.setLayoutManager(llm);
+
 
     }
 }
