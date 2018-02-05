bca8ab741c4ba3a4ee0066384f0e440dfe84b62f
afinievskym <afinievskym@gmail.com>
2018-02-05 22:41:57 +1000

    Пробую сделать (developerandroid) подключение бд через RecyclerView

---
+++ app/src/main/java/com/company/afinievskym/letsdoit/RecyclerView.java
@@ -0,0 +1,11 @@
+package com.company.afinievskym.letsdoit;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Programmer on 05.02.2018.
+ */
+
+public class RecyclerView extends AppCompatActivity {
+
+}
